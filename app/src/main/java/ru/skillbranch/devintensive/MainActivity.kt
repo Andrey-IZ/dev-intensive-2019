1ef6870c160771af6745d9c2f9165c72bef0d652
andrew <andrewvitorious@ya.ru>
2020-08-22 18:23:47 +0300

    FEATURE: added plural

---
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
